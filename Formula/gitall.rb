53bbb790b7241e0e73a04cb31ad27d796914086d
Jeremy Kassis <jkassis@gmail.com>
2023-01-25 12:19:38 -0800

    gitall ==> v0.8.11

--- Formula/gitall.rb
+++ Formula/gitall.rb
@@ -5,13 +5,13 @@
 class Gitall < Formula
   desc ""
   homepage "https://github.com/jkassis/gitall"
-  version "0.8.10"
+  version "0.8.11"
 
   on_macos do
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jkassis/gitall/releases/download/v0.8.10/gitall-darwin-10.10-amd64.tar.gz"
-      sha256 "120a067dbe1cad3d327af797ad3fa6ec24ed1cac20426f5d0fb8999f8abf1aff"
+      url "https://github.com/jkassis/gitall/releases/download/v0.8.11/gitall-darwin-10.10-amd64.tar.gz"
+      sha256 "984f7edbe560de793e5d0d75e8b961aea37e1d2312858862d29e82fcb7eb38a5"
 
       def install
         bin.install "gitall-darwin-10.10-amd64" => "gitall"
@@ -19,8 +19,8 @@ class Gitall < Formula
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jkassis/gitall/releases/download/v0.8.10/gitall-darwin-10.10-arm64.tar.gz"
-      sha256 "15e1dbbdcfc0da93657ddd647591db15e13083f1804adfd81b7daae97c012a50"
+      url "https://github.com/jkassis/gitall/releases/download/v0.8.11/gitall-darwin-10.10-arm64.tar.gz"
+      sha256 "d4090e087b84c7309fb1983de09b5de6772316fb9eff6091f6ec7815c4510c31"
 
       def install
         bin.install "gitall-darwin-10.10-arm64" => "gitall"
@@ -32,8 +32,8 @@ class Gitall < Formula
   on_linux do
 
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jkassis/gitall/releases/download/v0.8.10/gitall-linux-amd64.tar.gz"
-      sha256 "c5d04904a1f2c47404134f4756294dfc653db7f8b1302dcea72c9400d283bf76"
+      url "https://github.com/jkassis/gitall/releases/download/v0.8.11/gitall-linux-amd64.tar.gz"
+      sha256 "46a66e0a705ab42f24055d02be4e91fd0db9224a2e0ad14a38af24f9b37a3fa2"
 
       def install
         bin.install "gitall-linux-amd64" => "gitall"
@@ -41,8 +41,8 @@ class Gitall < Formula
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jkassis/gitall/releases/download/v0.8.10/gitall-linux-arm64.tar.gz"
-      sha256 "909ddc76d551269c4982e92704b9325a33987ac13acc142cca9e26fd8b05636f"
+      url "https://github.com/jkassis/gitall/releases/download/v0.8.11/gitall-linux-arm64.tar.gz"
+      sha256 "9b3cfff8271d5edae04abf7f01e8cd4a30393c20e76d0bdeeb7261655551fcc6"
 
       def install
         bin.install "gitall-linux-arm64" => "gitall"
