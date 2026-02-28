25f851c0b45377008f3f8b69d135fd71949f1437
Jeremy Kassis <jkassis@gmail.com>
2026-02-28 08:24:28 -0700

    Fix layout formula SHA256

--- Formula/layout.rb
+++ Formula/layout.rb
@@ -2,7 +2,7 @@ class Layout < Formula
   desc "macOS window layout manager with multi-space support and browser tab restore"
   homepage "https://github.com/jkassis/layout"
   url "https://github.com/jkassis/layout/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "cc874659735cd4389879b86d2245d86f5292d68a92a301a0e816e9d0ea3d9d58"
+  sha256 "22a95f8e061be686d88f1edecd9b540fa272233de462fab7dcdd94208c00245c"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
