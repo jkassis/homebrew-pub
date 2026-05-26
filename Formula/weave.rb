b064cf5c61575378261f5a872a6b033dc1c4fadb
Jeremy Kassis <jkassis@gmail.com>
2026-05-26 05:38:10 -0600

    update weave formula to 0.1.2

--- Formula/weave.rb
+++ Formula/weave.rb
@@ -1,22 +1,25 @@
 class Weave < Formula
   desc "Local-first terminal and agent collaboration TUI"
-  homepage "https://github.com/jkassis/weave"
-  version "0.1.1"
+  homepage "https://github.com/jkassis/homebrew-pub"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.1/weave-darwin-arm64-0.1.1.tar.gz"
-      sha256 "807115950aff541d424437942be427751f44669a628e91cf2e7e82fa19a82587"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.2/weave-darwin-arm64-0.1.2.tar.gz"
+      sha256 "81f5a77bd2cbe88bd3f724cf29056106692d575893b1bc4e1e0c49e8e90f5fa2"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.1/weave-darwin-amd64-0.1.1.tar.gz"
-      sha256 "04d74ded3ffc07ceed9a62fa48a39c052f71c473e66feb55db56a4b03697b103"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.2/weave-darwin-amd64-0.1.2.tar.gz"
+      sha256 "a9173239fcd074ff298893f2672e341b9a2ba2169b580c374b781fe05a65fa9b"
     end
   end
 
   def install
     bin.install "weave"
+  end
+
+  test do
+    assert_match "usage: weave", shell_output("#{bin}/weave --help 2>&1", 1)
   end
 end
