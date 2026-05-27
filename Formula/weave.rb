2ec6dabe3fb7826022351e7249e5b5b700274469
Jeremy Kassis <jkassis@gmail.com>
2026-05-26 18:34:59 -0600

    update weave formula to 0.1.3

--- Formula/weave.rb
+++ Formula/weave.rb
@@ -5,13 +5,13 @@ class Weave < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.2/weave-darwin-arm64-0.1.2.tar.gz"
-      sha256 "81f5a77bd2cbe88bd3f724cf29056106692d575893b1bc4e1e0c49e8e90f5fa2"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.3/weave-darwin-arm64-0.1.3.tar.gz"
+      sha256 "eea0b3aa4e0eba184a76ff19fabd0eec4f56d7436b20ba18f0477ced796030e5"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.2/weave-darwin-amd64-0.1.2.tar.gz"
-      sha256 "a9173239fcd074ff298893f2672e341b9a2ba2169b580c374b781fe05a65fa9b"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.3/weave-darwin-amd64-0.1.3.tar.gz"
+      sha256 "e7ab825295689d161c4ae2b59798a0880649bbcb615376dc3d1e273c5ec1a975"
     end
   end
 
