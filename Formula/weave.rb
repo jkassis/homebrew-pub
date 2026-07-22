e8d61508d11d585b7a948f7d180e6d967652f1f3
Jeremy Kassis <jkassis@gmail.com>
2026-07-22 12:02:45 -0600

    Update weave to 0.1.36

--- Formula/weave.rb
+++ Formula/weave.rb
@@ -5,13 +5,13 @@ class Weave < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.35/weave-darwin-arm64-0.1.35.tar.gz"
-      sha256 "0fe35cd1b7e99c4b49117c77a41bf10f21d9eb1e92726a907e305c2ea046610e"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.36/weave-darwin-arm64-0.1.36.tar.gz"
+      sha256 "cc582a1cd0e7e6426842d85ad796ee73357c70ca3748bb69c8a6b16b64196309"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.35/weave-darwin-amd64-0.1.35.tar.gz"
-      sha256 "4a7b9d1d9cebc4b6b2089fbacfbbf1d41532abdf9c547830949ad79fc384e945"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/weave-v0.1.36/weave-darwin-amd64-0.1.36.tar.gz"
+      sha256 "38092b0cea7c6f77329517d5365fb162ad9ef6cf21f7159abfb6c725f845d04b"
     end
   end
 
@@ -20,6 +20,6 @@ class Weave < Formula
   end
 
   test do
-    assert_match "weave 0.1.35", shell_output("#{bin}/weave --version")
+    assert_match "weave 0.1.36", shell_output("#{bin}/weave --version")
   end
 end
