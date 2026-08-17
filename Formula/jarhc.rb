1de78e1f3e88852521adb9260089a4eff74c49a8
Jeremy Kassis <jkassis@gmail.com>
2026-08-17 05:27:48 -0600

    Update JarHC to 0.1.3

--- Formula/jarhc.rb
+++ Formula/jarhc.rb
@@ -1,19 +1,19 @@
 class Jarhc < Formula
   desc "Terminal client for native Codex and Claude agent runtimes"
   homepage "https://github.com/jkassis/homebrew-pub"
-  version "0.1.2"
+  version "0.1.3"
 
   depends_on "node"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhc-dist-v0.1.2/jarhc-darwin-arm64-0.1.2.tar.gz"
-      sha256 "257107964becc656b7ba114af35a4d32e1251894e41e4794ac1f7511a1d4533d"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhc-dist-v0.1.3/jarhc-darwin-arm64-0.1.3.tar.gz"
+      sha256 "fd88be0701f53ebed629dbf68b9b263c81afc7acaed7f0c31314ac4ee567e536"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhc-dist-v0.1.2/jarhc-darwin-amd64-0.1.2.tar.gz"
-      sha256 "7470ebbc07d7b7fe9f5278f863134e4ad82dbba81309232318cb0353e8e057ac"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhc-dist-v0.1.3/jarhc-darwin-amd64-0.1.3.tar.gz"
+      sha256 "11376cd68d018e9da6ecb7f9b01a03c381a77bba8971a62ba506c19f4932291d"
     end
   end
 
@@ -23,6 +23,6 @@ class Jarhc < Formula
   end
 
   test do
-    assert_match "jarhc 0.1.2", shell_output("#{bin}/jarhc --version")
+    assert_match "jarhc 0.1.3", shell_output("#{bin}/jarhc --version")
   end
 end
