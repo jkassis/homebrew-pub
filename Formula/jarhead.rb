04a364e40bc5fd6dc7e5cf011aebee5656c1ec93
Jeremy Kassis <jkassis@gmail.com>
2026-08-24 15:32:51 -0600

    jarhead 0.1.24

--- Formula/jarhead.rb
+++ Formula/jarhead.rb
@@ -1,17 +1,17 @@
 class Jarhead < Formula
   desc "Terminal frontend for native Codex, Claude, and JarAgent runtimes"
-  homepage "https://github.com/jkassis/homebrew-pub"
+  homepage "https://github.com/jkassis/jarhead"
   depends_on "node"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhead-dist-v0.1.22/jarhead-darwin-arm64-0.1.22.tar.gz"
-      sha256 "3895f64c4a630ed068e5dab5dae66b1f488e8c5c0d93da9c02eecc2bb4cf94f0"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhead-dist-v0.1.24/jarhead-darwin-arm64-0.1.24.tar.gz"
+      sha256 "e1cf616ff736906f8e522377a0d1dbbad57fb8b7c403741099ec983955a3ecf4"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhead-dist-v0.1.22/jarhead-darwin-amd64-0.1.22.tar.gz"
-      sha256 "eea87f56c469065d21520fd833665d6b848ae3a316af78470e11bb385328a799"
+      url "https://github.com/jkassis/homebrew-pub/releases/download/jarhead-dist-v0.1.24/jarhead-darwin-amd64-0.1.24.tar.gz"
+      sha256 "db9c8071c09f6055ad300a71e4108fb8ef2a33b08c311a077dffcad288c54b1d"
     end
   end
 
@@ -21,6 +21,6 @@ class Jarhead < Formula
   end
 
   test do
-    assert_match "jarhead 0.1.22", shell_output("#{bin}/jarhead --version")
+    assert_match "jarhead 0.1.24", shell_output("#{bin}/jarhead --version")
   end
 end
